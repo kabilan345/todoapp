ae5ef126e98f5d906472aa13e3cf00340d2c1789
kabilan <kabilan.b2003@gmail.com>
2024-07-29 13:30:52 +0530

    timer

--- todo.js
+++ todo.js
@@ -17,14 +17,15 @@ function remove(event){
 var currentItem=null
 function change(event){
     currentItem=event.parentElement
-    var popup=document.getElementById("popup")
-    popup.style.display="block"
+    // var popup=document.getElementById("popup")
+    // popup.style.display="block"
+    currentItem.innerHTML=`<input type="text" id="editInput"> <button onclick="update2(event)">save</button> <button onclick="remove(event)">Delete</button>`
 }
 
 function update2(event){
-    var input2=document.getElementById("input2")
+    var input2=document.getElementById("editInput")
     var inputvalue=input2.value
-    currentItem.innerHTML=inputvalue+"<button onclick='remove(event)'>delete</button>"+" <button onclick='change(this)' id='edit_btn'>edit</button>"
-    popup.style.display="none"
+    currentItem.innerHTML=inputvalue+"<button onclick='remove(event)' id='delete_btn'>delete</button>"+" <button onclick='change(this)' id='edit_btn'>edit</button>"
+    // popup.style.display="none"
     input2.value=''
 }
